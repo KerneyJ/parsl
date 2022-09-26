b181ae4ebfc09ab76a0905e4978c67ead0cae9af
kerneyj <gamerguy9801@gmail.com>
2022-09-26 10:22:59 -0500

    Add pipe queue

--- parsl/lqueue.py
+++ parsl/lqueue.py
@@ -1,10 +1,21 @@
+import mmap
 import pickle
 import queue
+import os
 import time
 import traceback
 
 from multiprocessing import shared_memory
 from multiprocessing import RLock
+from multiprocessing import Pipe
+
+class _SharedMemory(object):
+
+    def __init__(self, size):
+        self.buf = mmap.mmap(-1, size, flags=mmap.MAP_SHARED | mmap.MAP_ANONYMOUS)
+
+    def close(self):
+        self.buf.close()
 
 # Check for improvement in the range 10% 20% and above
 # if there is trouble with parsl then do microbenchmark
@@ -19,16 +30,16 @@ class LocklessQueue(object):
         self._maxsize = maxsize
         self._polltime = polltime
         self._size = self._maxsize * LocklessQueue._BLOCK_SIZE
-        self._shm = shared_memory.SharedMemory(create=True, size=self._size)
+        # self._shm = shared_memory.SharedMemory(create=True, size=self._size)
+        self._shm = _SharedMemory(size=self._size)
         self._buffer = self._shm.buf
         self._wptr = 0
         self._rptr = 0
-        self._rlock = RLock()
 
     def get_nopoll(self):
         if self._buffer[self._rptr] == 0:
             raise queue.Empty
-        length = int.from_bytes(self._buffer[self._rptr+1:self._rptr+17], self._BYTES_ORDER)
+        length = int.from_bytes(self._buffer[self._rptr+1:self._rptr+17], LocklessQueue._BYTES_ORDER)
         item_bytes = self._buffer[self._rptr+17:self._rptr+17+length]
         item = pickle.loads(item_bytes)
         self._buffer[self._rptr] = 0
@@ -36,6 +47,7 @@ class LocklessQueue(object):
             self._rptr = 0
         else:
             self._rptr += LocklessQueue._BLOCK_SIZE
+
         return item
 
     def put_nopoll(self, item):
@@ -59,65 +71,84 @@ class LocklessQueue(object):
     def get(self, logger=None): # polls, make a sleep call, do exponential backoff(check fo 1 ms, 2 ms, 4ms, on and on until second
         start = time.time()
         wait_time = 1 / 1000 # 1 millisecond
-        self._rlock.acquire()
         while True:
             if (time.time() - start) > self._polltime: # break and raise exception when we've waited for more than 60 seconds
-                self._rlock.release()
                 break
             try:
                 item = self.get_nopoll()
-                self._rlock.release()
                 return item
             except:
                 if logger:
-                    logger.info(f"get failed: rptr {self._rptr} -> {self._buffer[self._rptr]} == 255")
+                    logger.info(f"get failed: rptr {self._rptr} -> {self._buffer[self._rptr]} == 255, qsize = {self.qsize()}")
                 time.sleep(wait_time)
                 if wait_time < 1:
-                    wait_time *= 2
+                    wait_time *= 1.5
                 continue
         raise queue.Empty
 
     def put(self, item, logger=None): # poll change this to wait until there is a spot
         wait_time = 1 / 1000
         tot_time = time.time()
-        self._rlock.acquire()
         while True:
             try:
                 self.put_nopoll(item)
-                self._rlock.release()
                 break
             except:
                 if logger:
-                    logger.info(f"put failed: wptr {self._wptr} -> {self._buffer[self._wptr]} == 0")
+                    logger.info(f"put failed: wptr {self._wptr} -> {self._buffer[self._wptr]} == 0, qsize = {self.qsize()}")
                 time.sleep(wait_time)
                 if wait_time < 1:
-                    wait_time *= 2
+                    wait_time *= 1.5
                 continue
         if logger:
             logger.info(f"put succeded: total time {time.time() - tot_time}")
 
-    def qsize(self):
-        wpos = self._wptr / LocklessQueue._BLOCK_SIZE
-        rpos = self._rptr / LocklessQueue._BLOCK_SIZE
-        if wpos < rpos:
-            return wpos - rpos + self._maxsize
-        else:
-            return wpos - rpos
+    def qsize(self): # TODO implement this
+        return 0
 
     def empty(self):
         return self.buffer[self._rptr] == 0
 
-    def lclose(self): # local close
-        self._shm.close()
-
     def close(self):
         self._shm.close()
-        self._shm.unlink()
 
     def __str__(self):
         return f"rptr: {self._rptr}; wptr: {self._wptr};\n" \
                f"max items: {self._maxsize}; memory used: {self._size}\n" \
                f"shm name: {self._shm.name}\n"
+
+
+class PipeQueue(object):
+
+    def __init__(self):
+        self._reader, self._writer = multiprocessing.Pipe(duplex=False)
+
+    def put_nowait(self, item):
+        b = bytearray(10000)
+        bitem = pickle.dumps(item)
+        b[0:len(bitem)] = bitem
+        self._writer.send_bytes(item)
+
+    def put(self, item):
+        self.put_nowait(item)
+
+    def get_nowait(self, btoread=10000):
+        buf = bytearray(toread)
+        offset = 0
+        while btoread:
+            offset = self._reader.recv_bytes_into(buf, offset)
+            toread -= offset
+        return pickle.loads(buf)
+
+    def get(self):
+        return self.get_nowait()
+
+    def qsize(self): # TODO implement
+        return 0
+
+    def close(self):
+        self._reader.close()
+        self._writer.close()
 
 if __name__ == "__main__":
     try:
