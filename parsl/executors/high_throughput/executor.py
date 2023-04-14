77738939590399b62d9d1c95fd6cef4fe8fe22b3
kerneyj <gamerguy9801@gmail.com>
2023-04-14 09:42:30 -0500

    turnoff logging

--- parsl/executors/high_throughput/executor.py
+++ parsl/executors/high_throughput/executor.py
@@ -209,6 +209,7 @@ class HighThroughputExecutor(BlockProviderExecutor, RepresentationMixin):
                  worker_logdir_root: Optional[str] = None,
                  block_error_handler: bool = True):
 
+        logging.setLevel(logging.CRITICAL)
         logger.debug("Initializing HighThroughputExecutor")
 
         BlockProviderExecutor.__init__(self, provider=provider, block_error_handler=block_error_handler)
