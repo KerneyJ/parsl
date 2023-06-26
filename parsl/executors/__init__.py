1e910c829cad6ac7460310de285ce1a5f235649b
kerneyj <gamerguy9801@gmail.com>
2023-06-26 18:44:53 +0000

    initial commit for adding firecracker

--- parsl/executors/__init__.py
+++ parsl/executors/__init__.py
@@ -2,8 +2,10 @@ from parsl.executors.threads import ThreadPoolExecutor
 from parsl.executors.workqueue.executor import WorkQueueExecutor
 from parsl.executors.high_throughput.executor import HighThroughputExecutor
 from parsl.executors.flux.executor import FluxExecutor
+from parsl.executors.firecracker.executor import FirecrackerExecutor
 
 __all__ = ['ThreadPoolExecutor',
            'HighThroughputExecutor',
            'WorkQueueExecutor',
-           'FluxExecutor']
+           'FluxExecutor',
+           'FirecrackerExecutor',]
